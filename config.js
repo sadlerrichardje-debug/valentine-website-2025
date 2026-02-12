fe669239e7d2ab4a9bb341a13381a1ebfa43ba2b
sadlerrichardje-debug <sadler.richardje@gmail.com>
2026-02-11 21:47:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/degsche1w/video/upload/v1770861477/Fridayy_-_When_It_Comes_To_You_ejb36r.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/degsche1w/video/upload/v1770864419/JayDon_Paradise_-_Lullaby_Official_Music_Video_iglwkl.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
